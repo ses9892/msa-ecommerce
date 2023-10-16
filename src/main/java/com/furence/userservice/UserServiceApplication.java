e7f9eab766b7427781af7c91db033735bdacc02b
jinho <ses1238@gmail.com>
2023-10-16 23:41:28 +0900

    user-service Eureka 서버에 등록

--- src/main/java/com/furence/userservice/UserServiceApplication.java
+++ src/main/java/com/furence/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.furence.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient          // Client Enabled
 public class UserServiceApplication {
 
     public static void main(String[] args) {
