2939b1c03e531e63a28b876e4e0768bad8717326
장진호 <ses1238@gmail.com>
2023-10-19 16:27:44 +0900

    Zuul Log Filter 생성 pre , post

--- src/main/java/com/furence/userservice/controller/HomeController.java
+++ src/main/java/com/furence/userservice/controller/HomeController.java
@@ -1,19 +1,29 @@
 package com.furence.userservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/user-service1")
+@Slf4j
 public class HomeController {
 
     @Value("${spring.application.name}")
     String appName;
 
     @GetMapping(value = "/welcome")
-    public String welcome(){
+    public String welcome(@RequestHeader("user-service1")String header, HttpServletRequest request){
+        String serialId = request.getParameter("serialId");
+
+        log.info("header value : {}" , header );
+        log.info("serialId value : {}" , serialId );
+
         return appName + " service welcome";
     }
 
